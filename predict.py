628ad7bced413d5ec81904c7945e998faa5aaae6
Jihwan Lim <ghwn5936@gmail.com>
2020-05-31 14:48:56 +0900

    Functionalize methods of digit predictor

--- predict.py
+++ predict.py
@@ -12,65 +12,69 @@ import torchvision
 from networks import MnistNetwork
 
 
-class DigitPredictor:
-    def __init__(self, model_path):
-        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        checkpoint = torch.load(model_path)
-        self.model = MnistNetwork().to(self.device)
-        self.model.load_state_dict(checkpoint)
-
-    def _create_image_object(self, image_bytes):
-        """Create a PIL.Image object that contains the image bytes."""
-        f = io.BytesIO()
-        f.write(image_bytes)
-        image = Image.open(f)
-        return image
-
-    def _to_jpeg(self, image_object, mode='RGB'):
-        """Convert the given PIL.Image object's format into `JPEG`."""
-        f = io.BytesIO()
-        image_object.convert(mode).save(f, format='JPEG')
-        image = Image.open(f)
-        return image
-
-    def _transform(self, image):
-        """Transforms the image to torch.Tensor."""
-        tensor = torchvision.transforms.ToTensor()(image)
-        tensor = torchvision.transforms.Normalize(mean=(0.1307,), std=(0.3081,))(tensor)
-        return tensor
-
-    def _preprocess(self, image_bytes):
-        """Preprocesses image bytes so that they can be input into the model.
-
-        Returns:
-            - a torch.Tensor of which shape is [1, 1, 28, 28].
-        """
-        image = self._create_image_object(image_bytes)
-        image = self._to_jpeg(image, mode='L')  # 'L' for grayscale
-        image = image.resize(size=(28, 28))
-        image = np.array(image, dtype=np.uint8)
-        tensor = self._transform(image)
-        tensor = tensor.unsqueeze(0)
-        return tensor
-
-    def predict(self, image_bytes):
-        """Predicts a digit the image represents."""
-        tensor = self._preprocess(image_bytes)
-        output = self.model(tensor.to(self.device))
-        prediction = output.argmax().item()
-        return prediction
+def create_image_object(image_bytes):
+    """Create a PIL.Image object that contains the image bytes."""
+    f = io.BytesIO()
+    f.write(image_bytes)
+    image = Image.open(f)
+    return image
 
 
-def main():
+def to_jpeg(image_object, mode='RGB'):
+    """Convert the given PIL.Image object's format into `JPEG`."""
+    f = io.BytesIO()
+    image_object.convert(mode).save(f, format='JPEG')
+    image = Image.open(f)
+    return image
+
+
+def transform(image):
+    """Transforms the image to torch.Tensor."""
+    tensor = torchvision.transforms.ToTensor()(image)
+    tensor = torchvision.transforms.Normalize(mean=(0.1307,), std=(0.3081,))(tensor)
+    return tensor
+
+
+def preprocess(image_bytes):
+    """Preprocesses image bytes so that they can be input into the model.
+
+    Returns:
+    - a torch.Tensor of which shape is [1, 1, 28, 28].
+    """
+    image = create_image_object(image_bytes)
+    image = to_jpeg(image, mode='L')  # 'L' for grayscale
+    image = image.resize(size=(28, 28))
+    image = np.array(image, dtype=np.uint8)
+    tensor = transform(image)
+    tensor = tensor.unsqueeze(0)
+    return tensor
+
+
+def predict(model_path, image_bytes):
+    """Predicts a digit the image represents."""
+    tensor = preprocess(image_bytes)
+
+    # load model
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    checkpoint = torch.load(model_path)
+    model = MnistNetwork().to(device)
+    model.load_state_dict(checkpoint)
+
+    output = model(tensor.to(device))
+    prediction = output.argmax().item()
+    return prediction
+
+
+def main(model_path, image_path):
+    with open(image_path, 'rb') as f:
+        image_bytes = f.read()
+    prediction = predict(model_path, image_bytes)
+    print('prediction: %s' % prediction)
+
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_path', type=str, required=True, help="path to MNIST model")
     parser.add_argument('--image', type=str, required=True, help="path to an image")
     args = parser.parse_args()
-
-    predictor = DigitPredictor(args.model_path)
-    prediction = predictor.predict(open(args.image, 'rb').read())
-    print('Prediction: %s' % prediction)
-
-
-if __name__ == "__main__":
-    main()
+    main(args.model_path, args.image)
